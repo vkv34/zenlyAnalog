987667bec3229847692edd021efd40305d201630
vkv34 <97910233+vkv34@users.noreply.github.com>
2023-05-16 01:02:04 +0300

    init

--- app/src/main/java/com/example/myapplication/maps/MapsFragment.kt
+++ app/src/main/java/com/example/myapplication/maps/MapsFragment.kt
@@ -4,6 +4,13 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
+import android.graphics.Rect
 import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -16,10 +23,25 @@ import androidx.fragment.app.Fragment
 import com.example.myapplication.R
 import com.example.myapplication.User
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.MapObjectCollection
+import com.yandex.mapkit.map.PlacemarkMapObject
 import com.yandex.mapkit.mapview.MapView
+import com.yandex.runtime.image.ImageProvider
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.URL
 
 
 /**
@@ -42,12 +64,23 @@ class MapsFragment : Fragment() {
     }
 
     private val mapKitFactory = MapKitFactory.getInstance()
+    private lateinit var mapObjects: MapObjectCollection
+    private var userObject: PlacemarkMapObject? = null
+
+    data class UserWithPlaceMark(
+        var user: User,
+        val placemarkMapObject: PlacemarkMapObject,
+    )
+
+    private var friendsMapObjectCollection: MutableList<UserWithPlaceMark> = mutableListOf()
+
+
     private lateinit var locationManager: LocationManager
 
     /**
      * Проверка на влключенный GPS
      */
-    private fun hasGps(): Boolean{
+    private fun hasGps(): Boolean {
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
     }
 
@@ -66,6 +99,9 @@ class MapsFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_maps, container, false)
         yandexMaps = view.findViewById(R.id.yandexMapView)
+        mapObjects = yandexMaps.map.mapObjects
+        getLocation()
+        getFriends()
         return view;
     }
 
@@ -79,8 +115,9 @@ class MapsFragment : Fragment() {
 
             permissionActivityResultLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
-        locationManager = requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        getLocation()
+        locationManager =
+            requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
 
     }
 
@@ -115,6 +152,7 @@ class MapsFragment : Fragment() {
         if (! checkPermissions())
             return
         val phone = Firebase.auth.currentUser?.phoneNumber
+
         if (hasGps()) {
             if (! checkPermissions())
                 return
@@ -124,30 +162,177 @@ class MapsFragment : Fragment() {
                 0f
             ) {
                 if (phone != null) {
-                    FirebaseDatabase.getInstance()
-                        .getReference("users")
-                        .child(phone)
-                        .child(User::lastLatitude.name)
-                        .setValue(it.latitude)
-                        .addOnSuccessListener { }
+
+                    if (userObject == null) {
+                        FirebaseDatabase.getInstance().getReference("users")
+                            .child(phone)
+                            .get()
+                            .addOnSuccessListener { snapshot ->
+                                val user = snapshot.getValue(User::class.java)
+                                if (user != null) {
+                                    CoroutineScope(Dispatchers.IO).launch {
+                                        getBitmapFromURL(user.imageUser)?.let { bitmap ->
+                                            withContext(Dispatchers.Main) {
+                                                userObject = mapObjects.addPlacemark(
+                                                    Point(user.lastLatitude, user.lastLongitude),
+                                                    ImageProvider.fromBitmap(getCroppedBitmap(bitmap))
+                                                )
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                    } else {
+                        userObject !!.geometry = Point(it.latitude, it.longitude)
+                    }
 
                     FirebaseDatabase.getInstance()
                         .getReference("users")
                         .child(phone)
-                        .child(User::lastLongitude.name)
-                        .setValue(it.latitude)
-                        .addOnSuccessListener { }
+                        .apply {
+                            child(User::lastLatitude.name)
+                                .setValue(it.latitude)
+                            child(User::lastLongitude.name)
+                                .setValue(it.longitude)
+                        }
 
                 }
             }
         }
     }
 
-    /* private fun getFriends():List<User>{
-         val usersRef = FirebaseDatabase.getInstance().getReference("users")
+    fun getBitmapFromURL(src: String?): Bitmap? {
+        return try {
+            val url = URL(src)
+            val connection =
+                url.openConnection() as HttpURLConnection
+            connection.doInput = true
+            connection.connect()
+            val input = connection.inputStream
+            val decodedBitmap = BitmapFactory.decodeStream(input)
+            Bitmap.createScaledBitmap(decodedBitmap, 120, 120, false)
+        } catch (e: IOException) {
+            // Log exception
+            null
+        }
+    }
 
-         usersRef.get().addOnSuccessListener {result->
+    fun getCroppedBitmap(bitmap: Bitmap): Bitmap? {
+        val output = Bitmap.createBitmap(
+            bitmap.width,
+            bitmap.height, Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(output)
+        val color = - 0xbdbdbe
+        val paint = Paint()
+        val rect = Rect(0, 0, bitmap.width, bitmap.height)
+        paint.isAntiAlias = true
+        canvas.drawARGB(0, 0, 0, 0)
+        paint.color = color
+        // canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        canvas.drawCircle(
+            (bitmap.width / 2).toFloat(), (bitmap.height / 2).toFloat(),
+            (
+                    bitmap.width / 2).toFloat(), paint
+        )
+        paint.setXfermode(PorterDuffXfermode(PorterDuff.Mode.SRC_IN))
+        canvas.drawBitmap(bitmap, rect, rect, paint)
+        //Bitmap _bmp = Bitmap.createScaledBitmap(output, 60, 60, false);
+        //return _bmp;
+        return output
+    }
 
-         }
-     }*/
+    private fun getFriends() {
+        val usersRef = FirebaseDatabase.getInstance().getReference("users")
+            .addValueEventListener(
+                object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+
+                        val users = mutableListOf<User>()
+                        for (postSnapshot in snapshot.children) {
+                            postSnapshot.getValue<User>()?.let { users.add(it) }
+                        }
+
+                        val usersOnMapList = users.filter {
+                            var contains = false
+                            friendsMapObjectCollection.forEach { friendList ->
+                                if (friendList.user.uuid == it.uuid) {
+                                    contains = true
+                                    return@forEach
+                                }
+                            }
+                            contains
+                        }
+
+                        val addedUsers = users.filter { user->
+                            var contains = false
+                            usersOnMapList.forEach {userOnMap->
+                                if(userOnMap.uuid == user.uuid){
+                                    contains = true
+                                    return@forEach
+                                }
+                            }
+                            !contains
+                        }
+                        val removedUsers = friendsMapObjectCollection.filter {
+                            var contains = false
+                            usersOnMapList.forEach { userOnMap ->
+                                if (userOnMap.uuid == it.user.uuid) {
+                                    contains = true
+                                    return@forEach
+                                }
+                            }
+                            !contains
+                        }
+
+                        removedUsers.forEach {
+                            mapObjects.remove(it.placemarkMapObject)
+                        }
+
+                        addedUsers.forEach { user ->
+                            CoroutineScope(Dispatchers.IO).launch {
+                                getBitmapFromURL(user.imageUser)?.let { bitmap ->
+                                    withContext(Dispatchers.Main) {
+                                        friendsMapObjectCollection.add(
+                                            UserWithPlaceMark(
+                                                user = user,
+                                                placemarkMapObject = mapObjects.addPlacemark(
+                                                    Point(
+                                                        user.lastLatitude,
+                                                        user.lastLongitude
+                                                    ),
+                                                    ImageProvider.fromBitmap(
+                                                        getCroppedBitmap(
+                                                            bitmap
+                                                        )
+                                                    )
+                                                )
+                                            )
+                                        )
+                                    }
+                                }
+                            }
+                        }
+
+                        friendsMapObjectCollection.forEach {
+                            it.user = users.first {
+                                p->p.uuid == it.user.uuid
+                            }
+                        }
+
+                        friendsMapObjectCollection.forEach {
+                            //mapObjects.remove(it.placemarkMapObject)
+//                            mapObjects.addPlacemark(Point(it.user.lastLatitude, it.user.lastLongitude))
+                            it.placemarkMapObject.geometry =
+                                Point(it.user.lastLatitude, it.user.lastLongitude)
+                        }
+
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+                }
+            )
+    }
 }
